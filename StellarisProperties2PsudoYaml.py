cf61911ca0ae2678a4cd45c2ad8b97ead6898376
WovNyx <36076725+WovNyx@users.noreply.github.com>
2018-02-03 20:51:30 +0900

    hotfix 1
    fixed crash when path.txt is unreadable or corrupted.
    added printing open/write filedir/name

--- StellarisProperties2PsudoYaml.py
+++ StellarisProperties2PsudoYaml.py
@@ -13,6 +13,7 @@ def Properties2ParadoxYaml(filename, path = None) :
     ##
     try :
         file = open(filedir + filename, "r", encoding = "UTF-8")
+        print("opening file {name} at {dir}".format( name = filename, dir = filedir)
     except :
         print("error opening properties file")
         print(filedir, filename)
@@ -31,6 +32,7 @@ def Properties2ParadoxYaml(filename, path = None) :
 
     try :
         file = open(filedir + filename[:-10] + "yml" , 'w', encoding = "UTF-8-SIG") 
+        print("writing file {name} at {dir}".format( name = filename[:-10]+"yml", dir = filedir)
     except :
         print("error making yaml-like paradox file")
         
@@ -45,6 +47,7 @@ def Properties2ParadoxYaml(filename, path = None) :
 
 if __name__ == "__main__" :
     ## path.txt is text file with 1 line of string which is path of target directory
+    path = ''
     try :
         target = open("./path.txt", 'r')
         path = target.read()
@@ -60,7 +63,7 @@ if __name__ == "__main__" :
                 print("bad path")
         print("found path", path)
     except :
-        path = None
+        path = './'
         print("properties path not found. using current directory")
     ## os.listdir(path) shows all thing in path directory
     ## os.path.isfile returns True if target(path+f in this case) is file
@@ -68,6 +71,5 @@ if __name__ == "__main__" :
     files = [f for f in os.listdir(path) if os.path.isfile(path+f ) ]
     print("files in this directory", files)
     for f in files:
-        if '.properties' == f[:-11] :
+        if '.properties' == f[-11:] :
             Properties2ParadoxYaml(f, path)
-    
