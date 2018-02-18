08ed823be72b98fb65f7dfe059e433a8dd8e5f8f
WovNyx <36076725+WovNyx@users.noreply.github.com>
2018-02-18 23:29:06 +0900

    Update ParadoxYml2JProb.py

--- ParadoxYml2JProb.py
+++ ParadoxYml2JProb.py
@@ -1,3 +1,51 @@
+import os
+
+def PY2JP(filename, path = None) :
+    
+    if path :
+        filedir = path
+    else :
+        filedir = os.getcwd()
+        
+        
+    try :
+        file = open(os.path.join(filedir, filename), "r", encoding = "UTF-8-SIG")
+        print("opening file {name} at {dir}".format( name = filename, dir = filedir) )
+    except :
+        print("error opening paradox file")
+        print(filedir, filename)
+        return False
+        
+    
+    recv = file.read().split("\n") ## seperate by line
+
+    proc = []
+
+    temp = None
+    if recv[0] == "l_english:" :
+        del recv[0]
+    else :
+        print("l_english not found on {}".format(filename) )
+        return False
+    
+    for line in recv :
+        temp = line[1:].split(" ", 1)
+        print(temp)
+        proc.append("=".join(temp))
+        
+    try :
+        ##open file as UTF-8-SIG(with BOM) for not crashing if it has BOM or not
+        file = open(os.path.join(filedir, filename[:-3]) + "properties" , 'w', encoding = "UTF-8-SIG") 
+        print("writing file {name} at {dir}".format( name = filename[:-3]+"properties", dir = filedir) )
+        for ii in proc :
+            file.write(ii + "\n")
+        file.close()
+    except :
+        print("error write properties file")
+        return False
+        
+    return True
+        
 if __name__ == "__main__" :
     ## path.txt is text file with 1 line of string which is path of target directory
     path = ''
@@ -28,9 +76,9 @@ if __name__ == "__main__" :
     notproced = []
     print("files in this directory", files)
     for f in files:
-        if '.properties' == f[-11:] :
+        if '.yml' == f[-4:] :
             try :
-                result = Properties2ParadoxYaml(f, path)
+                result = PY2JP(f, path)
                 print(result)
                 if result :
                     #if result['NoDelim'] != []:
